1decc279722289727f82d1bae9b4e0c57a3b797f
filip-mirazchiyski <124933131+filip-mirazchiyski@users.noreply.github.com>
2023-11-30 23:53:04 +0200

    Add files via upload

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,10 +1,12 @@
 import random
-
-computer_number = random.randint(1, 100)
+max_num = 100
+computer_number = random.randint(1, max_num)
 print(computer_number)
+guess_count = 3
 
-while True:
-    player_input = input("Guess the number: (1-100): ")
+while guess_count != 0:
+    player_input = input(f"Guess the number: (1-{max_num}): ")
+
 
     if not player_input.isdigit():
         print("Invalid input. Try again...")
@@ -14,8 +16,15 @@ while True:
 
     if player_number == computer_number:
         print("You guessed it!")
-        break
+        max_num += 100
+        computer_number = random.randint(1, max_num)
     elif player_number > computer_number:
         print("Too high!")
+        guess_count -= 1
     elif player_number < computer_number:
         print("Too low!")
+        guess_count -= 1
+
+    print(f"Guesses remaining: {guess_count}")
+    print()
+    print(computer_number)
